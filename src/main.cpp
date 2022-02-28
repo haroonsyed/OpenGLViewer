cb3d091830c1105c834a24643f7141c30f085173
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2022-02-28 17:16:59 -0500

    Finished part one of the assignment

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,19 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   vertexColor = vec4(aColor, 1.0);\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
+    "in vec4 vertexColor;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vertexColor;\n"
     "}\n\0";
 
 int main()
@@ -97,13 +101,13 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -.25f, 0.0f, // left  
-         0.5f, -.75f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f, // top   
+        -0.5f, -.25f, 0.0f, 1.0f, 0.0f, 0.0f, // left  
+         0.5f, -.75f, 0.0f, 0.0f, 1.0f, 0.0f,// right 
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f// top   
 
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f, -1.0f, 0.0f  // bottom
+        // -0.5f, -0.5f, 0.0f, // left  
+        //  0.5f, -0.5f, 0.0f, // right 
+        //  0.0f, -1.0f, 0.0f  // bottom
     };
     unsigned int numVertices = sizeof(vertices)/3;
 
@@ -116,8 +120,10 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // Position Data
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float))); // Color data
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
